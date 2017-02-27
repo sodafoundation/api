eb48c0a7b0d517d6bee924f37ccd0b0c1853dd14
Hui Wang <wanghui71@huawei.com>
2017-02-27 14:41:05 +0800

    Update REST API Spec and code

---
+++ api/openapi-spec/restapi/doc.go
@@ -0,0 +1,19 @@
+/*Package restapi OpenSDS
+
+    Schemes:
+      http
+    Host: localhost
+    BasePath: /
+    Version: v1
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
