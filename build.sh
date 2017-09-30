a091baf57edd92507c88a6143bc8fd9bb1cbe612
jerry <jay_wcom@163.com>
2017-09-30 11:41:07 +0800

    Add makefile to replace building function

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-go get github.com/opensds/opensds/cmd/osdslet
-go get github.com/opensds/opensds/cmd/osdsdock
-
-go install github.com/opensds/opensds/cmd/osdslet
-go install github.com/opensds/opensds/cmd/osdsdock
--- build_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-go get github.com/opensds/opensds/cmd/osdslet
-go get github.com/opensds/opensds/cmd/osdsdock
-
-(cd cmd/osdslet && ./build_docker.sh)
-(cd cmd/osdsdock && ./build_docker.sh)
