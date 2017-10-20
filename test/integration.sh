359d3d10425656f213697a4cec1f95e7ec4f880d
leonwanghui <wanghui71leon@gmail.com>
2017-10-20 16:49:15 +0800

    Fix some bugs

--- test/integration.sh
+++
@@ -1,8 +0,0 @@
-sudo docker build integration/osdsdock/ -t opensds/osdsdock:integration-test
-sudo docker run -d --net=host opensds/osdsdock:integration-test
-
-runTests() {
-  go test -v github.com/opensds/opensds/test/integration
-}
-
-runTests
