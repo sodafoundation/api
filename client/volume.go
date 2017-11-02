56daafeb56133ccec1fac3829387771f1e358846
leonwanghui <wanghui71leon@gmail.com>
2017-11-02 11:55:19 +0800

    Udate client package and add attachment operation test

--- client/volume.go
+++ client/volume.go
@@ -25,6 +25,11 @@ import (
 // could be discussed if it's better to define an interface.
 type VolumeBuilder *model.VolumeSpec
 
+// VolumeAttachmentBuilder contains request body of handling a volume request.
+// Currently it's assigned as the pointer of VolumeSpec struct, but it
+// could be discussed if it's better to define an interface.
+type VolumeAttachmentBuilder *model.VolumeAttachmentSpec
+
 // VolumeSnapshotBuilder contains request body of handling a volume snapshot
 // request. Currently it's assigned as the pointer of VolumeSnapshotSpec
 // struct, but it could be discussed if it's better to define an interface.
@@ -81,6 +86,48 @@ func (v *VolumeMgr) ListVolumes() ([]*model.VolumeSpec, error) {
 
 func (v *VolumeMgr) DeleteVolume(volID string, body VolumeBuilder) error {
 	url := v.Endpoint + "/v1alpha/block/volumes/" + volID
+
+	return v.Recv(request, url, "DELETE", body, nil)
+}
+
+func (v *VolumeMgr) CreateVolumeAttachment(body VolumeAttachmentBuilder) (*model.VolumeAttachmentSpec, error) {
+	var res model.VolumeAttachmentSpec
+	url := v.Endpoint + "/v1alpha/block/attachments"
+
+	if err := v.Recv(request, url, "POST", body, &res); err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+func (v *VolumeMgr) GetVolumeAttachment(atcID string) (*model.VolumeAttachmentSpec, error) {
+	var res model.VolumeAttachmentSpec
+	url := v.Endpoint + "/v1alpha/block/attachments/" + atcID
+
+	if err := v.Recv(request, url, "GET", nil, &res); err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+func (v *VolumeMgr) ListVolumeAttachments() ([]*model.VolumeAttachmentSpec, error) {
+	var res []*model.VolumeAttachmentSpec
+	url := v.Endpoint + "/v1alpha/block/attachments"
+
+	if err := v.Recv(request, url, "GET", nil, &res); err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (v *VolumeMgr) DeleteVolumeAttachment(atcID string, body VolumeAttachmentBuilder) error {
+	url := v.Endpoint + "/v1alpha/block/attachments/" + atcID
 
 	return v.Recv(request, url, "DELETE", body, nil)
 }
