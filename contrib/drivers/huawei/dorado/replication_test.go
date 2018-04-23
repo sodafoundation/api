43014c7b8f455b9800bb23ee433d2fbf1fa809d7
jerry <jay_wcom@163.com>
2018-04-23 17:30:27 +0800

    Remove some CI codes which are used for debuging

--- contrib/drivers/huawei/dorado/replication_test.go
+++ contrib/drivers/huawei/dorado/replication_test.go
@@ -15,7 +15,6 @@
 package dorado
 
 import (
-	"github.com/opensds/opensds/pkg/dock/proto"
 	"testing"
 )
 
@@ -23,74 +22,19 @@ func TestLoadConf(t *testing.T) {
 }
 
 func TestCreateReplication(t *testing.T) {
-	d := &ReplicationDriver{}
-	err := d.Setup()
-	if err != nil {
-		t.Error("set up ...", err)
-	}
-	opt := &proto.CreateReplicationOpts{
-		Id: "29693780-260a-47b7-91bc-bd03f0bf4f68",
-		PrimaryReplicationDriverData:   map[string]string{KLunId: "362"},
-		SecondaryReplicationDriverData: map[string]string{KLunId: "4761"},
-		ReplicationMode:                "ASYNC",
-	}
-	d.CreateReplication(opt)
 }
 
 func TestEnableReplication(t *testing.T) {
-	d := &ReplicationDriver{}
-	err := d.Setup()
-	if err != nil {
-		t.Error("set up ...", err)
-	}
-	opt := &proto.EnableReplicationOpts{
-		Id: "29693780-260a-47b7-91bc-bd03f0bf4f68",
-		PrimaryReplicationDriverData:   map[string]string{KLunId: "362"},
-		SecondaryReplicationDriverData: map[string]string{KLunId: "4761"},
-		Metadata:                       map[string]string{KPairId: "7079902c95e8000c"},
-	}
-	d.EnableReplication(opt)
 }
 
 func TestDisableReplication(t *testing.T) {
-	d := &ReplicationDriver{}
-	err := d.Setup()
-	if err != nil {
-		t.Error("set up ...", err)
-	}
-	opt := &proto.DisableReplicationOpts{
-		Id: "29693780-260a-47b7-91bc-bd03f0bf4f68",
-		PrimaryReplicationDriverData:   map[string]string{KLunId: "362"},
-		SecondaryReplicationDriverData: map[string]string{KLunId: "4761"},
-		Metadata:                       map[string]string{KPairId: "7079902c95e8000c"},
-	}
-	d.DisableReplication(opt)
+
 }
 
 func TestFailoverReplication(t *testing.T) {
-	d := &ReplicationDriver{}
-	err := d.Setup()
-	if err != nil {
-		t.Error("set up ...", err)
-	}
-	opt := &proto.FailoverReplicationOpts{
-		Id: "29693780-260a-47b7-91bc-bd03f0bf4f68",
-		PrimaryReplicationDriverData:   map[string]string{KLunId: "362"},
-		SecondaryReplicationDriverData: map[string]string{KLunId: "4761"},
-		Metadata:                       map[string]string{KPairId: "7079902c95e8000c"},
-	}
-	d.FailoverReplication(opt)
+
 }
 
 func TestDeleteReplication(t *testing.T) {
-	d := &ReplicationDriver{}
-	err := d.Setup()
-	if err != nil {
-		t.Error("set up ...", err)
-	}
-	opt := &proto.DeleteReplicationOpts{
-		Id:       "29693780-260a-47b7-91bc-bd03f0bf4f68",
-		Metadata: map[string]string{KPairId: "7079902c95e8000c"},
-	}
-	d.DeleteReplication(opt)
+
 }
