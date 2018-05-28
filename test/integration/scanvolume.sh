210f6c96761bd9d322805d0f011a49613f4fea78
lijuncloud <39259019+lijuncloud@users.noreply.github.com>
2018-05-28 12:46:08 +0800

    Update scanvolume.sh
    Modify the method of get Local ip Address

--- test/integration/scanvolume.sh
+++ test/integration/scanvolume.sh
@@ -1,15 +1,16 @@
 #!bin/bash
 
 CHKLOG=/var/log/scan/hkconfig.log
+#TARGETIP=10.10.3.156:3260
 ISCSILOG=/var/log/scan/iscsi.log
 TARGETLOG=/var/log/scan/target.log
 LOGINLOG=/var/log/scan/login.log
 SESSIONLOG=/var/log/scan/session.log
 DISKLOG=/var/log/scan/disk.log
 TARGETIPLOG=/var/log/scan/targetip.log
+DISKRSLOG=/var/log/scan/diskrs.log
 
 declare target
-declare TARGETIP
 
 #install chkconfig
 
@@ -28,7 +29,6 @@ while ( [ $? -ne 0 ] || [ `grep -c "newly installed" $CHKLOG` -ne 1 ] )
    done
 echo "sysv_rc_conf install finished"
 
-
 #link chkconfig
 cp /usr/sbin/sysv-rc-conf /usr/sbin/chkconfig
 if [ $? -eq 0  ];then
@@ -37,8 +37,7 @@ else
   echo "link chkconfig fail!"
 fi
 
-
-#GET TARGETIP  
+#GET TARGETIP 
 echo >ip.log
 echo >addr.log
 ifconfig |grep -w "inet"|grep addr >>addr.log
@@ -50,22 +49,25 @@ do
       echo $var | grep "addr">>ip.log
    done
 done
-##Get IP
+
+##Echo IP to log
 cat ip.log|while read line
 do
   TARGETIP=`echo $line | cut -d \: -f 2`
   TARGETIP=$TARGETIP:3260
   echo $TARGETIP>$TARGETIPLOG
-  break
 done
 
-#find target
-chkconfig iscsi on
-chkconfig iscsi --list > $ISCSILOG
-iscsiadm -m discovery -t sendtargets -p $TARGETIP >$TARGETLOG
+##get ip from log
+cat $TARGETIPLOG |while read line
+do
+   ip=`echo $line`
+   #find target
+  chkconfig iscsi on
+  chkconfig iscsi --list > $ISCSILOG
+  iscsiadm -m discovery -t sendtargets -p $ip>$TARGETLOG
 
-
-#login target
+  #login target
 cat $TARGETLOG |while read line
   do
      a=$line
@@ -73,7 +75,7 @@ cat $TARGETLOG |while read line
       target=`echo $a | cut -d \, -f 2` #iqn.2017-10.io.opensds:d3a3059d-7e31-4093-8c44-391528e748b0
       echo $target
   #login
-    iscsiadm -m node –T $TARGETIP，$target -l >$LOGINLOG
+    iscsiadm -m node –T $ip，$target -l >$LOGINLOG
    done
 
 if [ `grep -c "successful" $LOGINLOG` -eq 1 ];then
@@ -83,7 +85,6 @@ if [ `grep -c "successful" $LOGINLOG` -eq 1 ];then
    else
      echo "login target note fail!"
 fi
-
 #view login session
 iscsiadm -m session >$SESSIONLOG
 if [ `awk '{print NR}' $SESSIONLOG|tail -n1` -eq 1 ];then
@@ -92,14 +93,16 @@ fi
 
 #show disk info
 fdisk -l|grep Disk >$DISKLOG
-if [ `grep -c "Disk /dev/sdb: 1 GiB" $DISKLOG` -eq 1 ];then
-     echo "volume attachment successfully!"
-   else
-     echo "volume attachment fail!"
-fi
+#if [ `grep -c "Disk /dev/sd: 2 GiB" $DISKLOG` -eq 1 ];then
+#     echo "volume attachment successfully!"
+#   else
+#     echo "volume attachment fail!"
+#fi
+grep "Disk /dev/sd" $DISKLOG |grep "2 GiB">$DISKRSLOG
+
 
 #login out from the target 
-iscsiadm -m node –T $target -p $TARGETIP -u
+iscsiadm -m node –T $target -p $ip -u
 echo >$SESSIONLOG
 iscsiadm -m session >>$SESSIONLOG
 if [ -s $SESSIONLOG];then
@@ -107,3 +110,4 @@ if [ -s $SESSIONLOG];then
    else
      echo "LOGIN OUT SUCCESS"
 fi
+done
