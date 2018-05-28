16cef85f6b5784d7cb7b062d22866d2eb6f2c0af
lijuncloud <39259019+lijuncloud@users.noreply.github.com>
2018-05-28 20:20:03 +0800

    Update e2ef_test.go

--- test/e2e/e2ef_test.go
+++ test/e2e/e2ef_test.go
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build integration
+// +build e2e
 
-package integration
+package e2e
 
 import (
 	"bufio"
