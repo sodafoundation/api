c15f09b73da56f0412fb7ea6a12143102a042621
lixiaoy1 <xiaoyan.li@intel.com>
2018-06-05 12:23:10 -0400

    Create volume from snapshot
    Signed-off-by: Xiaoyan Li <xiaoyan.li@intel.com>

--- osdsctl/cli/volume.go
+++ osdsctl/cli/volume.go
@@ -74,6 +74,7 @@ var (
 	volName   string
 	volDesp   string
 	volAz     string
+	volSnap   string
 )
 
 func init() {
@@ -83,6 +84,7 @@ func init() {
 	volumeCreateCommand.Flags().StringVarP(&volName, "name", "n", "", "the name of created volume")
 	volumeCreateCommand.Flags().StringVarP(&volDesp, "description", "d", "", "the description of created volume")
 	volumeCreateCommand.Flags().StringVarP(&volAz, "az", "a", "", "the availability zone of created volume")
+	volumeCreateCommand.Flags().StringVarP(&volSnap, "snapshot", "s", "", "the snapshot to create volume")
 	volumeCommand.AddCommand(volumeShowCommand)
 	volumeCommand.AddCommand(volumeListCommand)
 	volumeCommand.AddCommand(volumeDeleteCommand)
@@ -114,6 +116,7 @@ func volumeCreateAction(cmd *cobra.Command, args []string) {
 		AvailabilityZone: volAz,
 		Size:             int64(size),
 		ProfileId:        profileId,
+		SnapshotId:       volSnap,
 	}
 
 	resp, err := client.CreateVolume(vol)
@@ -135,7 +138,7 @@ func volumeShowAction(cmd *cobra.Command, args []string) {
 		Fatalln(HttpErrStrip(err))
 	}
 	keys := KeyList{"Id", "CreatedAt", "UpdatedAt", "Name", "Description", "Size",
-		"AvailabilityZone", "Status", "PoolId", "ProfileId", "Metadata", "GroupId"}
+		"AvailabilityZone", "Status", "PoolId", "ProfileId", "Metadata", "GroupId", "SnapshotId"}
 	PrintDict(resp, keys, FormatterList{})
 }
 
