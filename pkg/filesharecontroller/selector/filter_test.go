54d98d91f2591fa1a0f6beaacd83f8e330b35f9a
Shruthi MN <48908765+Shruthi-1MN@users.noreply.github.com>
2019-04-18 19:27:07 +0530

    Update filter_test.go

--- pkg/filesharecontroller/selector/filter_test.go
+++ pkg/filesharecontroller/selector/filter_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Huawei Technologies Co., Ltd. All Rights Reserved.
+// Copyright (c) 2019 OpenSDS Authors. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+// ## Refactor work will need to be done to consolidate the filter for block and file share
 /*
 This module implements the policy-based scheduling by parsing storage
 profiles configured by admin.
