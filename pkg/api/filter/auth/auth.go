9ae20d61665e99713fa01863bd71f64279993f0a
leonwanghui <wanghui71leon@gmail.com>
2019-04-19 16:01:41 +0800

    Update some method handles in apiserver
    Signed-off-by: leonwanghui <wanghui71leon@gmail.com>

--- pkg/api/filter/auth/auth.go
+++ pkg/api/filter/auth/auth.go
@@ -27,8 +27,11 @@ type AuthBase interface {
 	Filter(ctx *context.Context)
 }
 
-type NoAuth struct {
+func NewNoAuth() AuthBase {
+	return &NoAuth{}
 }
+
+type NoAuth struct{}
 
 func (auth *NoAuth) Filter(httpCtx *context.Context) {
 	ctx := c.GetContext(httpCtx)
@@ -38,10 +41,6 @@ func (auth *NoAuth) Filter(httpCtx *context.Context) {
 		ctx.IsAdmin = true
 	}
 	httpCtx.Input.SetData("context", ctx)
-}
-
-func NewNoAuth() AuthBase {
-	return &NoAuth{}
 }
 
 func Factory() beego.FilterFunc {
