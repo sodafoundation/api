c7e430a341fbe0a475f03a7410ff653a290e88a3
PrakashR <rhsakarpos@gmail.com>
2019-04-22 04:35:20 -0400

    updated mock client

--- testutils/controller/testing/client.go
+++ testutils/controller/testing/client.go
@@ -451,6 +451,36 @@ func (_m *Client) FailoverReplication(ctx context.Context, in *proto.FailoverRep
 	return r0, r1
 }
 
+// GetMetrics provides a mock function with given fields: ctx, in, opts
+func (_m *Client) GetMetrics(ctx context.Context, in *proto.GetMetricsOpts, opts ...grpc.CallOption) (*proto.GenericResponse, error) {
+	_va := make([]interface{}, len(opts))
+	for _i := range opts {
+		_va[_i] = opts[_i]
+	}
+	var _ca []interface{}
+	_ca = append(_ca, ctx, in)
+	_ca = append(_ca, _va...)
+	ret := _m.Called(_ca...)
+
+	var r0 *proto.GenericResponse
+	if rf, ok := ret.Get(0).(func(context.Context, *proto.GetMetricsOpts, ...grpc.CallOption) *proto.GenericResponse); ok {
+		r0 = rf(ctx, in, opts...)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*proto.GenericResponse)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, *proto.GetMetricsOpts, ...grpc.CallOption) error); ok {
+		r1 = rf(ctx, in, opts...)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // UpdateVolumeGroup provides a mock function with given fields: ctx, in, opts
 func (_m *Client) UpdateVolumeGroup(ctx context.Context, in *proto.UpdateVolumeGroupOpts, opts ...grpc.CallOption) (*proto.GenericResponse, error) {
 	_va := make([]interface{}, len(opts))
