7788b1bcf6951131210cbd0d9549681974e8c516
SatyaG <satya.g@huawei.com>
2019-04-24 18:02:26 +0530

    Updated CI shell scripts copyright

--- test/e2e/e2eflowtest.sh
+++ test/e2e/e2eflowtest.sh
@@ -1,4 +1,6 @@
-#Copyright 2019 The OpenSDS Authors.
+#!/bin/bash
+
+# Copyright 2019 The OpenSDS Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- test/e2e/e2etest.sh
+++ test/e2e/e2etest.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Copyright 2019 The OpenSDS Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
