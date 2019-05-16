bc61ce00955815242044e0875610adedcccf538d
Tak <d.a.takuzone@gmail.com>
2019-05-16 11:28:59 +0900

    add nimble driver into init and clean

--- contrib/drivers/drivers.go
+++ contrib/drivers/drivers.go
@@ -24,6 +24,7 @@ package drivers
 import (
 	_ "github.com/opensds/opensds/contrib/backup/multicloud"
 	"github.com/opensds/opensds/contrib/drivers/ceph"
+	"github.com/opensds/opensds/contrib/drivers/hpe/nimble"
 	"github.com/opensds/opensds/contrib/drivers/huawei/dorado"
 	"github.com/opensds/opensds/contrib/drivers/huawei/fusionstorage"
 	"github.com/opensds/opensds/contrib/drivers/lvm"
@@ -99,6 +100,9 @@ func Init(resourceType string) VolumeDriver {
 		break
 	case config.HuaweiFusionStorageDriverType:
 		d = &fusionstorage.Driver{}
+	case config.HpeNimbleDriverType:
+		d = &nimble.Driver{}
+		break
 	default:
 		d = &sample.Driver{}
 		break
@@ -120,6 +124,8 @@ func Clean(d VolumeDriver) VolumeDriver {
 	case *dorado.Driver:
 		break
 	case *fusionstorage.Driver:
+		break
+	case *nimble.Driver:
 		break
 	default:
 		break
