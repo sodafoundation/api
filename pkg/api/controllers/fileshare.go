sodafoundation/api

commit 330df4a7d5f9e76a313059b61cb9d8ed78f0b23f

fix for #811 and some validation (#855)
* Fix for issue#832 and some validation

* Fix some validation and optimize the code

pkg/api/controllers/fileshare.go
@@ -500,25 +500,21 @@ func (f *FileShareSnapshotPortal) CreateFileShareSnapshot() {
 		return
 	}
 
+	var err error
+	fileshare, err := db.C.GetFileShare(ctx, snapshot.FileShareId)
+	if err != nil {
+		errMsg := fmt.Sprintf("fileshare %s not found: %s", snapshot.FileShareId, err.Error())
+		f.ErrorHandle(model.ErrorNotFound, errMsg)
+		return
+	}
+
 	// Get profile
 	var prf *model.ProfileSpec
-	var err error
-	if snapshot.ProfileId == "" {
-		log.Warning("Use default profile when user doesn't specify profile.")
-		prf, err = db.C.GetDefaultProfileFileShare(ctx)
-		if err != nil {
-			errMsg := fmt.Sprintf("get profile failed: %s", err.Error())
-			f.ErrorHandle(model.ErrorBadRequest, errMsg)
-			return
-		}
-		snapshot.ProfileId = prf.Id
-	} else {
-		prf, err = db.C.GetProfile(ctx, snapshot.ProfileId)
-		if err != nil {
-			errMsg := fmt.Sprintf("get profile failed: %s", err.Error())
-			f.ErrorHandle(model.ErrorBadRequest, errMsg)
-			return
-		}
+	prf, err = db.C.GetProfile(ctx, fileshare.ProfileId)
+	if err != nil {
+		errMsg := fmt.Sprintf("get profile failed: %s", err.Error())
+		f.ErrorHandle(model.ErrorBadRequest, errMsg)
+		return
 	}
 
 	// NOTE:It will create a fileshare snapshot entry into the database and initialize its status

pkg/api/util/db.go
@@ -172,6 +172,12 @@ func CreateFileShareSnapshotDBEntry(ctx *c.Context, in *model.FileShareSnapshotS
 		in.CreatedAt = time.Now().Format(constants.TimeFormat)
 	}
 
+	//validate the snapshot name
+	if strings.HasPrefix(in.Name, "snapshot") {
+		errMsg := fmt.Sprintf("Names starting 'snapshot' are reserved. Please choose a different snapshot name.")
+		log.Error(errMsg)
+		return nil, errors.New(errMsg)
+	}
 	in.Status = model.FileShareSnapCreating
 	in.Metadata = fshare.Metadata
 	return db.C.CreateFileShareSnapshot(ctx, in)

pkg/controller/controller.go
@@ -973,13 +973,8 @@ func (c *Controller) CreateFileShareAcl(contx context.Context, opt *pb.CreateFil
 		db.UpdateFileShareStatus(ctx, db.C, opt.Id, model.FileShareError)
 		return pb.GenericResponseError(err), err
 	}
-	polInfo, err := c.selector.SelectSupportedPoolForFileShare(fileshare)
-	if err != nil {
-		db.UpdateFileShareStatus(ctx, db.C, opt.Id, model.FileShareError)
-		return pb.GenericResponseError(err), err
-	}
 
-	dockInfo, err := db.C.GetDock(ctx, polInfo.DockId)
+	dockInfo, err := db.C.GetDockByPoolId(ctx, fileshare.PoolId)
 	if err != nil {
 		db.UpdateFileShareStatus(ctx, db.C, opt.Id, model.FileShareError)
 		log.Error("when search supported dock resource:", err.Error())
@@ -1012,13 +1007,7 @@ func (c *Controller) DeleteFileShareAcl(contx context.Context, opt *pb.DeleteFil
 		db.UpdateFileShareStatus(ctx, db.C, opt.Id, model.FileShareError)
 		return pb.GenericResponseError(err), err
 	}
-	polInfo, err := c.selector.SelectSupportedPoolForFileShare(fileshare)
-	if err != nil {
-		db.UpdateFileShareStatus(ctx, db.C, opt.Id, model.FileShareError)
-		return pb.GenericResponseError(err), err
-	}
-
-	dockInfo, err := db.C.GetDock(ctx, polInfo.DockId)
+	dockInfo, err := db.C.GetDockByPoolId(ctx, fileshare.PoolId)
 	if err != nil {
 		db.UpdateFileShareStatus(ctx, db.C, opt.Id, model.FileShareError)
 		log.Error("when search supported dock resource:", err.Error())

commit fb5b43aab7381da5fae6b2a9af52b525dd7c4f4c

Fix issue #866 caused by #855

pkg/api/controllers/fileshare.go
@@ -531,6 +531,7 @@ func (f *FileShareSnapshotPortal) CreateFileShareSnapshot() {
 		f.ErrorHandle(model.ErrorBadRequest, errMsg)
 		return
 	}
+	snapshot.ProfileId = prf.Id
 
 	// NOTE:It will create a fileshare snapshot entry into the database and initialize its status
 	// as "creating". It will not wait for the real fileshare snapshot creation to complete
@@ -669,7 +670,7 @@ func (f *FileShareSnapshotPortal) DeleteFileShareSnapshot() {
 
 	prf, err := db.C.GetProfile(ctx, snapshot.ProfileId)
 	if err != nil {
-		errMsg := fmt.Sprintf("delete snapshot failed: %v", err.Error())
+		errMsg := fmt.Sprintf("profile (%s) not found: %v", snapshot.ProfileId, err.Error())
 		f.ErrorHandle(model.ErrorBadRequest, errMsg)
 		return
 	}
@@ -678,7 +679,7 @@ func (f *FileShareSnapshotPortal) DeleteFileShareSnapshot() {
 	// the database to "deleting" and return the result immediately.
 	err = util.DeleteFileShareSnapshotDBEntry(ctx, snapshot)
 	if err != nil {
-		errMsg := fmt.Sprintf("delete file share snapshot failed: %v", err.Error())
+		errMsg := fmt.Sprintf("delete file share snapshot in db failed: %v", err.Error())
 		f.ErrorHandle(model.ErrorBadRequest, errMsg)
 		return
 	}