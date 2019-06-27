a89b83b0a0cff659b5ddc3ad598d94a10d7fcc48
kimura <kimura.hiroki@jp.fujitsu.com>
2019-06-27 20:00:04 +0900

    Add fujitsu ETERNUS DX driver prototype

--- pkg/utils/config/config_define.go
+++ pkg/utils/config/config_define.go
@@ -96,6 +96,7 @@ type Backends struct {
 	HpeNimble           BackendProperties `conf:"hpe_nimble"`
 	NFS                 BackendProperties `conf:"nfs"`
 	Manila              BackendProperties `conf:"manila"`
+	FujitsuEternus      BackendProperties `conf:"fujitsu_eternus"`
 }
 
 type KeystoneAuthToken struct {
