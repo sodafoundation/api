674bd8fa25c04fd703a18bca4e70c895c53eaea4
SatyaG <satya.g@huawei.com>
2019-08-30 11:55:41 +0530

    IntegrationTest framework files

---
+++ test/integration/fileshare_test.go
@@ -0,0 +1,45 @@
+// Copyright 2019 The OpenSDS Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package integration
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	utils "github.com/opensds/opensds/test/integration/utils"
+)
+
+func TestFileShare(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "FileShare Suite")
+}
+
+var _ = Describe("FileShare Testing", func() {
+
+	Context("create FileShare", func() {
+		// var jsonStr = []byte(`{"name":"share2", "description":"This is just for test2", "size": 2}`)
+		payLoad := "http://127.0.0.1:50040/v1beta/e93b4c0934da416eb9c8d120c5d04d96/file/shares" //curl -X POST -H "Content-Type: application/json" -d '{"name":"share1", "description":"This is just for test", "size": 1}' -url "http://127.0.0.1:50040/v1beta/e93b4c0934da416eb9c8d120c5d04d96/file/shares"
+		methodName := "GET"
+
+		utils.ConnectToHttpGet(methodName, payLoad)
+		// It("fileshare get", func() {
+		// 	payLoad := "http://127.0.0.1:50040/v1beta/e93b4c0934da416eb9c8d120c5d04d96/file/shares" //curl -X POST -H "Content-Type: application/json" -d '{"name":"share1", "description":"This is just for test", "size": 1}' -url "http://127.0.0.1:50040/v1beta/e93b4c0934da416eb9c8d120c5d04d96/file/shares"
+		// 	methodName := "PUT"
+		// 	utils.ConnectToHttp(methodName, payLoad, jsonStr)
+		// })
+		// It("has 0 units", func() {})
+		// Specify("the total amount is 0.00", func() {})
+	})
+})
