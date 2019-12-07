45d4388c9e7515bfcf3927fa748786a15a1f81ff
wisererik <lynheell@gmail.com>
2019-12-07 15:57:42 +0530

    Remove unreasonable validation of fileshare description

--- pkg/api/util/db.go
+++ pkg/api/util/db.go
@@ -193,18 +193,6 @@ func CreateFileShareDBEntry(ctx *c.Context, in *model.FileShareSpec) (*model.Fil
 		log.Error(errMsg)
 		return nil, errors.New(errMsg)
 	}
-	// validate the description
-	reg, err = regexp.Compile("[^a-zA-Z0-9 ]+")
-	if err != nil {
-		errMsg := fmt.Sprintf("regex compilation for file share description validation failed")
-		log.Error(errMsg)
-		return nil, errors.New(errMsg)
-	}
-	if reg.MatchString(in.Description) {
-		errMsg := fmt.Sprintf("invalid fileshare description and it has some special characters")
-		log.Error(errMsg)
-		return nil, errors.New(errMsg)
-	}
 
 	in.UserId = ctx.UserId
 	in.Status = model.FileShareCreating
