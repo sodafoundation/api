34e357bc7f224ce0bc6ad4a8955fd785f64b4cd2
Vaibhav Ghadge <vaibhav.ghadge@agrostar.in>
2020-01-08 10:33:15 +0530

    added check for fileshare snapshot description special char not allowed

--- pkg/api/util/db.go
+++ pkg/api/util/db.go
@@ -293,6 +293,17 @@ func CreateFileShareSnapshotDBEntry(ctx *c.Context, in *model.FileShareSnapshotS
 		log.Error(errMsg)
 		return nil, errors.New(errMsg)
 	}
+	reg, err := regexp.Compile("^[a-zA-Z0-9 _-]+$")
+	if err != nil {
+		errMsg := fmt.Sprintf("regex compilation for file share snapshot validation failed")
+		log.Error(errMsg)
+		return nil, errors.New(errMsg)
+	}
+	if reg.MatchString(in.Description) == false {
+		errMsg := fmt.Sprintf("invalid file share snapshot description it only contain english char and number  : %v", in.Name)
+		log.Error(errMsg)
+		return nil, errors.New(errMsg)
+	}
 	in.Status = model.FileShareSnapCreating
 	in.Metadata = fshare.Metadata
 	return db.C.CreateFileShareSnapshot(ctx, in)
