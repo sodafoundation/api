1c88ea57a494ebae259b9e311801c64ad6da6910
ThisIsClark <liuyuchibubao@gmail.com>
2020-01-17 00:08:21 +0800

    Update unit test

--- pkg/dock/discovery/discovery_test.go
+++ pkg/dock/discovery/discovery_test.go
@@ -95,10 +95,16 @@ func TestDiscover(t *testing.T) {
 		"Name":   {SamplePools[1].Name},
 		"DockId": {""},
 	}
+	m3 := map[string][]string{
+		"Name":   {SamplePools[2].Name},
+		"DockId": {""},
+	}
+
 	mockClient := new(dbtest.Client)
 	mockClient.On("ListPools", c.NewAdminContext()).Return(fdd.pols, nil)
 	mockClient.On("ListPoolsWithFilter", c.NewAdminContext(), m1).Return(expected, nil)
 	mockClient.On("ListPoolsWithFilter", c.NewAdminContext(), m2).Return(expected, nil)
+	mockClient.On("ListPoolsWithFilter", c.NewAdminContext(), m3).Return(expected, nil)
 	fdd.c = mockClient
 
 	if err := fdd.Discover(); err != nil {
